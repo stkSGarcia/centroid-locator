0df1832b27c149116c31d1d4d37ae998ebe0e9ca
Samuel Garcia <stk.sgarcia@gmail.com>
2024-10-09 13:54:58 -0400

    update login & support register

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
 import logging
-from typing import Optional
+from typing import Optional, Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Form
 from pydantic import BaseModel
 
 from app.dao import user as user_dao
@@ -14,23 +14,35 @@ router = APIRouter(
 )
 
 
-class User(BaseModel):
+class LoginData(BaseModel):
     username: str
     passwd: str
 
 
-class LoginResponse(BaseModel):
+class RegisterData(BaseModel):
+    username: str
+    name: str
+    password: str
+
+
+class UserResponse(BaseModel):
     code: int
     data: Optional[dict] = None
     msg: Optional[str] = None
 
 
 @router.post("/login")
-async def login(user: User) -> LoginResponse:
-    code, name = user_dao.login(user.username, user.passwd)
+async def login(data: Annotated[LoginData, Form()]) -> UserResponse:
+    code, name = user_dao.login(username=data.username, passwd=data.passwd)
     if code == 1:
-        return LoginResponse(code=0, data={"username": user.username, "name": name})
+        return UserResponse(code=0, data={"username": data.username, "name": name})
     elif code == 0:
-        return LoginResponse(code=0, msg="密码错误！")
+        return UserResponse(code=0, msg="密码错误！")
     else:
-        return LoginResponse(code=0, msg="当前用户不存在！")
+        return UserResponse(code=0, msg="当前用户不存在！")
+
+
+@router.post("/register")
+async def register(data: Annotated[RegisterData, Form()]) -> UserResponse:
+    is_successful = user_dao.register(username=data.username, name=data.name, passwd=data.password)
+    return UserResponse(code=1) if is_successful else UserResponse(code=0)
